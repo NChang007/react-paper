8936339d3f1e0794e1c8c8f1f5deee4d6062940d
Nelson Chang <nelsonc924@gmail.com>
2023-09-18 17:51:44 -0700

    update to css and added title

--- src/paper/Paper.jsx
+++ src/paper/Paper.jsx
@@ -4,7 +4,8 @@ import './paper.css'
 const Paper = () => {
   return (
     <div className='notebook'>
-        <div className="paper">
+        <div className="paper" id={1}>
+            <p className='paper-title'>Title Of Paper</p>
             <div className="paper-content">
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sit quidem, dicta possimus repudiandae quaerat voluptates, beatae atque aperiam ullam temporibus cupiditate pariatur architecto at reiciendis, praesentium expedita sint sapiente.
