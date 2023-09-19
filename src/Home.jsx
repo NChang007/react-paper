fc3632b26da4e5dbcea2d7bcf57af6fc9b944191
Nelson Chang <nelsonc924@gmail.com>
2023-09-18 17:02:47 -0700

    the paper looks like a paper

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Paper from './paper/Paper'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Paper></Paper>
+    </>
+  )
+}
+
+export default Home
---
+++ src/paper/Paper.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './paper.css'
+
+const Paper = () => {
+  return (
+    <div className='notebook'>
+        <div className="paper">
+            <div className="paper-content">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sit quidem, dicta possimus repudiandae quaerat voluptates, beatae atque aperiam ullam temporibus cupiditate pariatur architecto at reiciendis, praesentium expedita sint sapiente.
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Paper
